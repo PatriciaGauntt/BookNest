f1a78465901cb63002a68c772fd86391e4adb7ed
curiouskarma <plgauntt@hotmail,com>
2025-11-22 18:34:15 -0600

    stying

--- src/app/book-details/book-details.ts
+++ src/app/book-details/book-details.ts
@@ -10,7 +10,7 @@ import { BookService } from '../book.service';
   templateUrl: './book-details.html',
   styleUrl: './book-details.css',
 })
-export class BookDetails {
+/*export class BookDetails {
   bookService: BookService = inject(BookService);
   router: Router = inject(Router);
   route: ActivatedRoute = inject(ActivatedRoute);
@@ -39,6 +39,52 @@ export class BookDetails {
   await this.bookService.deleteBook(this.bookId);
   this.router.navigate(['/books/search']);
   };
+}*/
+export class BookDetails {
+  bookService: BookService = inject(BookService);
+  router: Router = inject(Router);
+  route: ActivatedRoute = inject(ActivatedRoute);
+  bookId: string = '';
+  currentBook!: BookType;
+
+  // ⭐ NEW: Flag used to show/hide modal
+  showDeleteModal = false;
+
+  commentForm = new FormGroup({
+    name: new FormControl(''),
+    comment: new FormControl(''),
+  });
+
+  constructor() {
+    this.bookId = this.route.snapshot.params['id'];
+    this.bookService.getBookById(this.bookId).then((book) => {
+      this.currentBook = book;
+    });
+  }
+
+  submitComment() {
+    this.bookService.submitComment(
+      this.commentForm.value.name ?? '',
+      this.commentForm.value.comment ?? ''
+    );
+  }
+
+  // ⭐ NEW: Open the modal instead of confirm()
+  confirmDelete() {
+    this.showDeleteModal = true;
+  }
+
+  // ⭐ NEW: Close modal without deleting
+  closeModal() {
+    this.showDeleteModal = false;
+  }
+
+  // ⭐ NEW: Called when user clicks "Yes, Delete"
+  async deleteBook() {
+    await this.bookService.deleteBook(this.bookId);
+    this.showDeleteModal = false;
+    this.router.navigate(['/books/search']);
+  }
 }
 
 
